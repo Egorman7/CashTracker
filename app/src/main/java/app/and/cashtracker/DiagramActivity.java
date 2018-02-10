a93435e74d81cdcb5611b8ea18a3d7155c4e3f9f
shaiytan <postscriptum105@gmail.com>
2018-02-10 03:41:09 +0200

    replace imagebutton bicycles with standard menus

--- app/src/main/java/app/and/cashtracker/DiagramActivity.java
+++ app/src/main/java/app/and/cashtracker/DiagramActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +22,6 @@ import app.and.cashtracker.database.DBHelper;
 
 public class DiagramActivity extends AppCompatActivity {
     private AnimatedPieView mChart;
-    private ImageButton mButton;
     private TextView mTextView;
     private boolean isIncome;
     private AnimatedPieViewConfig configOutcome, configIncome;
@@ -38,29 +37,28 @@ public class DiagramActivity extends AppCompatActivity {
         isIncome = false;
         dateEnd = DBHelper.SDF.format(Calendar.getInstance().getTime());
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_YEAR,-14);
+        cal.add(Calendar.DAY_OF_YEAR, -14);
         dateStart = DBHelper.SDF.format(cal.getTime());
 
         initializeView();
         initializeOutcomeConfig();
         initializeIncomeConfig();
         initializeData();
-        initializeListeners();
 
     }
 
-    private void initializeView(){
+    private void initializeView() {
         mTextView = findViewById(R.id.diagram_info);
         mChart = findViewById(R.id.diagram_pie);
-        mButton = findViewById(R.id.diagram_renew);
     }
-    private void initializeIncomeConfig(){
-        Map<String, Float> dataMap = DBHelper.getValuesForCategoriesByDates(DBHelper.getInstance(this),dateStart,dateEnd,true);
+
+    private void initializeIncomeConfig() {
+        Map<String, Float> dataMap = DBHelper.getValuesForCategoriesByDates(DBHelper.getInstance(this), dateStart, dateEnd, true);
         configIncome = new AnimatedPieViewConfig();
         String[] keys2 = dataMap.keySet().toArray(new String[dataMap.size()]);
-        for (int i=0; i<keys2.length; i++){
-            if(dataMap.get(keys2[i])>0){
-                configIncome.addData(new SimplePieInfo(dataMap.get(keys2[i]),DBHelper.getCategoryColor(DBHelper.getInstance(this),keys2[i]),keys2[i]));
+        for (String aKeys2 : keys2) {
+            if (dataMap.get(aKeys2) > 0) {
+                configIncome.addData(new SimplePieInfo(dataMap.get(aKeys2), DBHelper.getCategoryColor(DBHelper.getInstance(this), aKeys2), aKeys2));
             }
         }
         configIncome.setStartAngle(-90);
@@ -72,17 +70,19 @@ public class DiagramActivity extends AppCompatActivity {
         configIncome.setOnPieSelectListener(new OnPieSelectListener<IPieInfo>() {
             @Override
             public void onSelectPie(@NonNull IPieInfo pieInfo, boolean isScaleUp) {
-                if(isScaleUp) Toast.makeText(getApplicationContext(),pieInfo.getDesc() + ": " + pieInfo.getValue() + " UAH", Toast.LENGTH_SHORT).show();
+                if (isScaleUp)
+                    Toast.makeText(getApplicationContext(), pieInfo.getDesc() + ": " + pieInfo.getValue() + " UAH", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private void initializeOutcomeConfig(){
-        Map<String, Float> dataMap = DBHelper.getValuesForCategoriesByDates(DBHelper.getInstance(this),dateStart,dateEnd,false);
+
+    private void initializeOutcomeConfig() {
+        Map<String, Float> dataMap = DBHelper.getValuesForCategoriesByDates(DBHelper.getInstance(this), dateStart, dateEnd, false);
         configOutcome = new AnimatedPieViewConfig();
         String[] keys = dataMap.keySet().toArray(new String[dataMap.size()]);
-        for(int i=0; i<keys.length; i++){
-            if(dataMap.get(keys[i])>0)
-                configOutcome.addData(new SimplePieInfo(dataMap.get(keys[i]),DBHelper.getCategoryColor(DBHelper.getInstance(this),keys[i]),keys[i]));
+        for (String key : keys) {
+            if (dataMap.get(key) > 0)
+                configOutcome.addData(new SimplePieInfo(dataMap.get(key), DBHelper.getCategoryColor(DBHelper.getInstance(this), key), key));
         }
         configOutcome.setStartAngle(-90);
         configOutcome.setTextSize(32);
@@ -94,35 +94,41 @@ public class DiagramActivity extends AppCompatActivity {
         configOutcome.setOnPieSelectListener(new OnPieSelectListener<IPieInfo>() {
             @Override
             public void onSelectPie(@NonNull IPieInfo pieInfo, boolean isScaleUp) {
-                if(isScaleUp) Toast.makeText(getApplicationContext(),pieInfo.getDesc() + ": " + pieInfo.getValue() + " UAH", Toast.LENGTH_SHORT).show();
+                if (isScaleUp)
+                    Toast.makeText(getApplicationContext(), pieInfo.getDesc() + ": " + pieInfo.getValue() + " UAH", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    private void initializeData(){
+
+    private void initializeData() {
         mChart.applyConfig(configOutcome);
         mChart.start();
-        mTextView.setText("Расходы (" + dateStart + " - " + dateEnd+")");
+        mTextView.setText("Расходы (" + dateStart + " - " + dateEnd + ")");
     }
-    private void initializeListeners(){
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isIncome = !isIncome;
-                if(isIncome){
-                    mTextView.setText("Доходы (" + dateStart + " - " + dateEnd+")");
-                    mChart.applyConfig(configIncome);
-                } else {
-                    mTextView.setText("Расходы (" + dateStart + " - " + dateEnd+")");
-                    mChart.applyConfig(configOutcome);
-                }
-                mChart.start();
-            }
-        });
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.renew_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        isIncome = !isIncome;
+        if (isIncome) {
+            mTextView.setText("Доходы (" + dateStart + " - " + dateEnd + ")");
+            mChart.applyConfig(configIncome);
+        } else {
+            mTextView.setText("Расходы (" + dateStart + " - " + dateEnd + ")");
+            mChart.applyConfig(configOutcome);
+        }
+        mChart.start();
+        return true;
     }
 
     @Override
     public void finish() {
         super.finish();
-        overridePendingTransition(0,R.anim.activity_journal_zoom_out);
+        overridePendingTransition(0, R.anim.activity_journal_zoom_out);
     }
 }
